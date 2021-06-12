a5899c9323aa21e4c6d34ddbeee13eccfd7f4a4c
Sean O'Reilly <seansoreilly@gmail.com>
2021-06-12 14:10:26 +1000

    Initial commit

---
+++ src/taskpane/components/App.tsx
@@ -0,0 +1,98 @@
+import * as React from "react";
+import { Button, ButtonType } from "office-ui-fabric-react";
+import Header from "./Header";
+import HeroList, { HeroListItem } from "./HeroList";
+import Progress from "./Progress";
+// images references in the manifest
+import "../../../assets/icon-16.png";
+import "../../../assets/icon-32.png";
+import "../../../assets/icon-80.png";
+
+export interface AppProps {
+  title: string;
+  isOfficeInitialized: boolean;
+}
+
+export interface AppState {
+  listItems: HeroListItem[];
+}
+
+export default class App extends React.Component<AppProps, AppState> {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      listItems: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      listItems: [
+        {
+          icon: "Ribbon",
+          primaryText: "Achieve more with Office integration",
+        },
+        {
+          icon: "Unlock",
+          primaryText: "Unlock features and functionality",
+        },
+        {
+          icon: "Design",
+          primaryText: "Create and visualize like a pro",
+        },
+      ],
+    });
+  }
+
+  click = async () => {
+    /**
+     * Insert your Outlook code here
+     */
+
+    let sTo: Office.Recipients = Office.context.mailbox.item.to;
+     let salutation: string = "";
+     let firstName:string = "";
+     
+     for (let t of sTo) {
+       firstName = t.displayName.split(" ", 1); 
+       salutation = salutation + firstName + " and";
+     }
+     
+     //remove last "and"
+     salutation = salutation.substr(0,salutation.length - 3);
+     
+     salutation = "Hi " + salutation;
+     
+     console.log(salutation);
+
+  };
+
+  render() {
+    const { title, isOfficeInitialized } = this.props;
+
+    if (!isOfficeInitialized) {
+      return (
+        <Progress title={title} logo="assets/logo-filled.png" message="Please sideload your addin to see app body." />
+      );
+    }
+
+    return (
+      <div className="ms-welcome">
+        <Header logo="assets/logo-filled.png" title={this.props.title} message="Welcome" />
+        <HeroList message="Discover what Office Add-ins can do for you today!" items={this.state.listItems}>
+          <p className="ms-font-l">
+            Modify the source files, then click <b>Run</b>.
+          </p>
+          <Button
+            className="ms-welcome__action"
+            buttonType={ButtonType.hero}
+            iconProps={{ iconName: "ChevronRight" }}
+            onClick={this.click}
+          >
+            Run
+          </Button>
+        </HeroList>
+      </div>
+    );
+  }
+}
