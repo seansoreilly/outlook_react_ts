6e250f0f82ae0e3ad7dbcf5d07beab425e39bc4b
Sean O'Reilly <seansoreilly@gmail.com>
2021-06-12 23:32:47 +1000

    Salutation function working!

--- src/taskpane/components/Functions.tsx
+++ src/taskpane/components/Functions.tsx
@@ -1,28 +1,33 @@
 
 
-  export var salutation = function(emailTo: Office.Recipients) {
+  // export var salutation = function(emailTo: Office.Recipients) {
+  export var salutation = function(emailTo) {
   // export function salutation() {
 
     // let emailTo: Office.Recipients = Office.context.mailbox.item.to;
 
-     let salutation: string = "";
-     let firstName: string = "";
-     let counter: number = 0;
+     var salutation: string = "";
+     var fullName: string = "";
+     var firstName: string = "";
+     var countOfTo:number = 0;
+     var toEnumerator:number = 0;
+     countOfTo  = emailTo.lastIndex;
 
-     for (var t in emailTo) {
-    //  for (let t in emailTo["displayName"]) {
-      let fullName =  emailTo[counter]["displayName"];
+    //  for (let toEnumerator = 0, toEnumerator < countOfTo; toEnumerator++) {
+     for (toEnumerator = 0; toEnumerator <= countOfTo; toEnumerator) {
+      fullName =  emailTo[toEnumerator++]["displayName"];
       firstName = fullName.split(" ", 1).toString(); 
-       salutation = salutation + firstName + " and";
+       salutation = salutation + " " + firstName + " and";
      }
      
      //remove last "and"
-     salutation = salutation.substr(0,salutation.length - 3);
+     var salutationLength:number = 0;
+     salutationLength = salutation.length;
+
+     salutation = salutation.substr(1,salutationLength - 5);
      
      salutation = "Hi " + salutation;
      
-     console.log(salutation);
-
      return salutation;
 
   };
