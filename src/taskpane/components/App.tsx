84bb81a8aec19578af683c4bb69fd19caef1369e
Sean O'Reilly <seansoreilly@gmail.com>
2021-06-26 19:27:31 +1000

    API not working yet

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -49,11 +49,55 @@ export default class App extends React.Component<AppProps, AppState> {
   }
 
   click = async () => {
+
     /**
      * Insert your Outlook code here
      */
 
-    var getSalutation:string = Functions.salutation(Office.context.mailbox.item.to);
+    var configAws = { invokeUrl: 'https://comprehend.us-east-2.amazonaws.com/' }
+    var apigClient = Aws.apigClientFactory.newClient(configAws);
+    var _result;
+    var pathTemplate = '/users/{userID}/profile'
+    var method = 'GET';    
+
+    var params = {
+      // This is where any modeled request parameters should be added.
+      // The key is the parameter name, as it is defined in the API in API Gateway.
+      param0: '',
+      param1: ''
+    };
+
+    var body = {
+      // This is where you define the body of the request,
+      "LanguageCode": "en",
+      "Text": "{\n    \"Text\": \"TO THE CHIEF EXECUTIVE OFFICER Sent to CEOs and CEO EAs  Dear Colleagues, MAV State Council Meeting – 'Save the Date' – Friday 21 May 2020 The next MAV State Council Meeting will be held from 9.30am to 2:30pm on Friday 21 May 2021 at the Melbourne Town Hall, Corner Swanston and Collins Streets Melbourne.  The online links will be available to submit motions and register to attend in mid-March and in accordance with the MAV Rules, motions are to be submitted no later than midnight on Friday 23 April 2021.\n}"
+    };
+
+    var additionalParams = {
+      // If there are any unmodeled query parameters or headers that must be
+      //   sent with the request, add them here.
+      headers: {
+        param0: '',
+        param1: ''
+      },
+      queryParams: {
+        param0: '',
+        param1: ''
+      }
+    };
+
+    apigClient.invokeApi(params, method, pathTemplate, additionalParams, body)
+      .then(function (_result) {
+        // Add success callback code here.
+        console.log("success");
+      }).catch(function (_result) {
+        // Add error callback code here.
+        console.log("error");
+      });
+
+    console.log(_result);
+
+    var getSalutation: string = Functions.salutation(Office.context.mailbox.item.to);
     console.log(getSalutation);
     Commands.putNotificationMessage(getSalutation);
 
